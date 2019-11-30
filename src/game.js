d0afb6aa8fd3f793be5f71a828565080b9406847
Your Name <you@example.com>
2019-11-30 15:32:38 +0530

    Passing Data Through state ...

--- src/game.js
+++ src/game.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
-
 class Square extends React.Component {
-  
+  constructor(props){
+    super(props);
+    this.state={
+      value:null,
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {this.props.value}
-
+      <button className="square" onClick={()=>this.setState({value:'X'})}>
+        {this.state.value}
+        {/* {this.props.value} */}
+        
       </button>
     );
   }
@@ -43,7 +48,4 @@ class Board extends React.Component{
   }
 }
 export default Board;
-
-// Passing Data Through Props ...
-
 
